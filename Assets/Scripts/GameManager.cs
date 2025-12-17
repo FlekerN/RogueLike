fd22d940ce7e5a0adc58b4b4f5d56d76865eaeae
FlekerN <bgarridotornero@gmail.com>
2025-12-17 02:09:08 +0100

    roguelike

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+public class GameManager : MonoBehaviour
+{
+    public UIDocument UIDoc;
+    private Label m_FoodLabel;
+   public static GameManager Instance { get; private set; }
+   public BoardManager BoardManager;
+   public PlayerController PlayerController;
+   public TurnManager TurnManager { get; private set;}
+    private int m_FoodAmount = 100;
+   private void Awake()
+   {
+       if (Instance != null)
+       {
+           Destroy(gameObject);
+           return;
+       }
+      
+       Instance = this;
+   }
+  
+   void Start()
+    {
+        m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel");
+        m_FoodLabel.text = "Food : " + m_FoodAmount;
+        TurnManager = new TurnManager();
+        TurnManager.OnTick += OnTurnHappen;
+        
+        BoardManager.Init();
+        PlayerController.Spawn(BoardManager, new Vector2Int(1,1));
+    }
+    void OnTurnHappen()
+    {
+        ChangeFood(-1);
+    }
+    public void ChangeFood(int amount)
+    {
+        m_FoodAmount += amount;
+        m_FoodLabel.text = "Food : " + m_FoodAmount;
+    }
+}
---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TurnManager
+{
+    public event System.Action OnTick;
+    private int m_TurnCount;
+
+   public TurnManager()
+   {
+       m_TurnCount = 1;
+   }
+
+   public void Tick()
+   {
+        if(OnTick != null)
+        {
+            OnTick.Invoke();
+        }
+        m_TurnCount += 1;
+        Debug.Log("Current turn count : " + m_TurnCount);
+   }
+}
